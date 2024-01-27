bf5ed3145a10503d4b18cc0f3bb2ac050d828e07
Anuj Dhungana <hoomandaws1999@gmail.com>
2024-01-27 15:15:15 +0545

    add script for burger menu

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+let navBurger = document.querySelector(".nav-burger");
+let closeIcon = document.querySelector(".close");
+let drawer = document.querySelector(".drawer");
+let overlayer = document.querySelector(".overlayer");
+navBurger.addEventListener("click", function () {
+  drawer.classList.add("open");
+  overlayer.classList.remove("hidden");
+});
+const close = function () {
+  drawer.classList.remove("open");
+  overlayer.classList.add("hidden");
+};
+closeIcon.addEventListener("click", close);
+overlayer.addEventListener("click", close);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && drawer.classList.contains("open")) {
+    close();
+  }
+});
